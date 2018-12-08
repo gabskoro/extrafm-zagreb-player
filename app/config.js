956d1d8669122b6f35ee92618642acc5c22774f6
Gabrijel Škoro <gabrijel.skoro@gmail.com>
2018-12-08 23:36:42 +0100

    update stream host

--- app/config.js
+++ app/config.js
@@ -1,7 +1,7 @@
 const Config = {
-  HOST: 'http://78.46.19.154:8110/stream',
+  HOST: 'http://streams.extrafm.hr:8110/stream',
   // Cross origin is not allowed, so I'm using a proxy service to bypass it
-  CURRENT_SONG_HOST: 'https://cors.io/?http://78.46.19.154:8110/currentsong',
+  CURRENT_SONG_HOST: 'https://cors.io/?http://streams.extrafm.hr:8110/currentsong',
   // I think every 1 minute is enough
   CURRENT_SONG_REFRESH_RATE: 1 * 60 * 1000
 }
