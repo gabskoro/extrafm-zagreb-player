f281bf65202d6536b00063c19142bbd3706f4d10
Gabrijel Škoro <gabrijel.skoro@gmail.com>
2018-12-17 16:44:09 +0100

    update dependencies

--- webpack-utils/webpack.common.js
+++ webpack-utils/webpack.common.js
@@ -21,7 +21,7 @@ const config = {
                 use: {
                     loader: 'babel-loader',
                     options: {
-                        presets: 'babel-preset-env'
+                        presets: ['@babel/preset-env']
                     }
                 },
                 exclude: /node_modules/
