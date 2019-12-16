de8f88805d298372d976d359e93d15d7eabcb786
Gabrijel Škoro <gabrijel.skoro@gmail.com>
2019-12-16 22:06:51 +0100

    feat: use my own proxy server now

--- app/config.js
+++ app/config.js
@@ -1,10 +1,11 @@
 const Config = {
-  HOST: 'http://streams.extrafm.hr:8110/stream',
+  HOST: "http://streams.extrafm.hr:8110/stream",
   // Couldn't find any reliable proxy service so,
   // cross origin was allowed adding the webSecurity property in the main.js
-  CURRENT_SONG_HOST: 'http://streams.extrafm.hr:8110/currentsong',
+  CURRENT_SONG_HOST:
+    "http://proxy.gabrijelskoro.com/?url=http://streams.extrafm.hr:8110/currentsong",
   // I think every 1 minute is enough
   CURRENT_SONG_REFRESH_RATE: 1 * 60 * 1000
-}
+};
 
 export default Config;
