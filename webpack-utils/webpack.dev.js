54097eedb441de3dc3e1240573c3f4da271ed1fe
Gabrijel Škoro <gabrijel.skoro@gmail.com>
2019-12-16 22:34:01 +0100

    chore: update dependencies:wq

--- webpack-utils/webpack.dev.js
+++ webpack-utils/webpack.dev.js
@@ -1,33 +1,34 @@
 const commonPaths = require('./common-paths');
 
 const config = {
-    devtool: 'eval',
-    mode: 'development',
-    output: {
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.scss$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader'
-                ]
-            },
-            {
-                test: /\.(svg|png)$/,
-                use: {
-                    loader: 'file-loader',
-                    options: {
-                        name: '[name].[ext]',
-                        outputPath: commonPaths.imagesOutputPath
-                    }
-                }
-            }
+  devtool: 'eval',
+  mode: 'development',
+  output: {
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
         ]
-    }
+      },
+      {
+        test: /\.(svg|png)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+              name: '[name].[ext]',
+              outputPath: commonPaths.imagesOutputPath,
+              esModule: false
+          }
+        }
+      }
+    ]
+  }
 }
 
 module.exports = config;
--- webpack-utils/webpack.prod.js
+++ webpack-utils/webpack.prod.js
@@ -5,48 +5,49 @@ const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const commonPaths = require('./common-paths');
 
 const config = {
-    devtool: 'source-map',
-    mode: 'production',
-    output: {
-        filename: 'bundle.[hash].js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.scss$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    'sass-loader'
-                ]
-            },
-            {
-                test: /\.(svg|png)$/,
-                use: {
-                    loader: 'file-loader',
-                    options: {
-                        outputPath: commonPaths.imagesOutputPath
-                    }
-                }
-            }
+  devtool: 'source-map',
+  mode: 'production',
+  output: {
+    filename: 'bundle.[hash].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
         ]
-    },
-    plugins: [
-        new MiniCssExtractPlugin('bundle.[hash].css'),
-        new Webpack.optimize.ModuleConcatenationPlugin(),
-        new UglifyJsPlugin({
-            sourceMap: true
-        }),
-        new OptimizeCSSAssetsPlugin({
-            cssProcessorOptions: {
-                map: {
-                  inline: false
-                }
-            }
-        }),
-        // Stripping logs/warnings from vue
-        new Webpack.DefinePlugin({ 'process.env': { NODE_ENV: '"production"' }})
+      },
+      {
+        test: /\.(svg|png)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            outputPath: commonPaths.imagesOutputPath,
+            esModule: false
+          }
+        }
+      }
     ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin('bundle.[hash].css'),
+    new Webpack.optimize.ModuleConcatenationPlugin(),
+    new UglifyJsPlugin({
+      sourceMap: true
+    }),
+    new OptimizeCSSAssetsPlugin({
+      cssProcessorOptions: {
+        map: {
+          inline: false
+        }
+      }
+    }),
+    // Stripping logs/warnings from vue
+    new Webpack.DefinePlugin({ 'process.env': { NODE_ENV: '"production"' }})
+  ]
 }
 
 module.exports = config;
